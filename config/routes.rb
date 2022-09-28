0d3acb5481b2df9663b5f47efbe535838bc0184e
Alan Ramalho <ramalho.sit@gmail.com>
2022-09-27 22:49:27 -0300

    current page check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :visitantes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "visitantes#index"
 end
